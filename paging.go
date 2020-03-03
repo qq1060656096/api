4ae62b5e74919e5ec52a997df5b0a9adf89a9b73
赵思贵 <1060656096@qq.com>
2020-03-03 23:10:16 +0800

    ccc: 修改文件名

--- paging.go
+++ paging.go
@@ -1,12 +1,12 @@
 package api
 
-type PagingLimit struct {
+type Paging struct {
 	Page     uint64 `json:"page" form:"page" valid:"int~分页必须在1-2100000000之间,range(1|2100000000)~分页必须在1-2100000000之间"`
 	PageSize uint64 `json:"pageSize" form:"pageSize" valid:"int~分页大小必须在1-2100000000之间,range(1|2100000000)~分页大小必须在1-2100000000之间"`
 }
 
-func NewPagingLimit(page uint64, pageSize uint64) *PagingLimit {
-	return &PagingLimit{
+func NewPaging(page uint64, pageSize uint64) *Paging {
+	return &Paging{
 		Page:     page,
 		PageSize: pageSize,
 	}
